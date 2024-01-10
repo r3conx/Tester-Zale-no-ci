61a5c2eeac9a50378352ffe2db5d34bceb337d2c
r3conx <michutmpl@gmail.com>
2024-01-10 01:38:35 +0000

    10 sty 2024, 02:38

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ function getDynamicDependencies() {
 function generateString() {
     updateDynamicDependencies();
     const inputStrings = document.getElementById('inputStrings').value.split(',');
-    const selectedDependencies = Array.from(document.querySelectorAll('.dependency'))
+    const selectedDependencies = Array.from(document.querySelectorAll('.dependency input:checked'))
                                       .map(dep => window[dep.id.replace('check-', '')])
                                       .filter(dep => typeof dep === 'function');
 
