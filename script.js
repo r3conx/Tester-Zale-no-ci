70dea1e2b1a014c9d5424249229b7557d6d390fd
r3conx <michutmpl@gmail.com>
2024-01-10 03:33:26 +0000

    10 sty 2024, 04:33

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ function generateString() {
     let generatedString = '';
     let isStringValid;
     let attempts = 0;
-    const limit = 100000;
+    const limit = 1000000;
 
     do {
         generatedString = generateRandomString(inputStrings[0].length);
