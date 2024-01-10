2362faec98c35729e50ceaba0b84a2cb16cf635b
r3conx <michutmpl@gmail.com>
2024-01-10 04:39:12 +0000

    10 sty 2024, 05:39

--- script.js
+++ script.js
@@ -194,17 +194,13 @@ window.findSumDependencies = function(strings) {
     let stringDependencies = strings.map(string => {
         let deps = new Set();
 
-        // Sprawdzanie kombinacji sumy dowolnych cyfr i porównywanie z innymi cyframi
         for (let i = 0; i < length; i++) {
             for (let j = i + 1; j < length; j++) {
-                let sum = 0;
+                let sum = parseInt(string[i], 10) + parseInt(string[j], 10);
                 for (let k = 0; k < length; k++) {
-                    if (k !== i && k !== j) {
-                        sum += parseInt(string[k], 10);
+                    if (k !== i && k !== j && sum % 10 === parseInt(string[k], 10)) {
+                        deps.add(`sumOf${i}${j}equals${k}`);
                     }
-                }
-                if (sum % 10 === parseInt(string[j], 10)) {
-                    deps.add(`sumExcept${i}${j}equals${j}`);
                 }
             }
         }
@@ -226,6 +222,23 @@ window.findSumDependencies = function(strings) {
 
     return dynamicDepFunctions;
 };
+
+function createDynamicFunction(dep, length) {
+    let match = dep.match(/sumOf(\d+)(\d+)equals(\d+)/);
+    if (match) {
+        let firstIndex = parseInt(match[1], 10);
+        let secondIndex = parseInt(match[2], 10);
+        let targetIndex = parseInt(match[3], 10);
+
+        return function(testStrings) {
+            return testStrings.map(string => {
+                let sum = parseInt(string[firstIndex], 10) + parseInt(string[secondIndex], 10);
+                return sum % 10 === parseInt(string[targetIndex], 10);
+            });
+        };
+    }
+}
+
 
 function createDynamicFunction(dep, length) {
     let match = dep.match(/sumExcept(\d+)(\d+)equals(\d+)/);
