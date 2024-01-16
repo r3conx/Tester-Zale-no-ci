836f051b31d78aa26bd8a2c7f90a84fcc93b0ca7
r3conx <michutmpl@gmail.com>
2024-01-16 19:41:25 +0000

    16 sty 2024, 20:41

--- script.js
+++ script.js
@@ -149,14 +149,14 @@ let sum = 0;
 for (let k = sumStartIndex; k < string.length; k++) {
 if (k !== targetIndex) {
 sum += parseInt(string[k], 10);
-console.log(sum,"sum");
-console.log(string[k],"string[k]");
-console.log(parseInt(string[k], 10),"parseInt(string[k], 10)");
+//console.log(sum,"sum");
+//console.log(string[k],"string[k]");
+//console.log(parseInt(string[k], 10),"parseInt(string[k], 10)");
 }
 }
-console.log(parseInt(string[targetIndex], 10),"parseInt(string[targetIndex], 10)");
-console.log(sum % 10,"sum % 10");
-console.log(parseInt(string[targetIndex], 10) === sum % 10,"parseInt(string[targetIndex], 10) === sum % 10");
+//console.log(parseInt(string[targetIndex], 10),"parseInt(string[targetIndex], 10)");
+//console.log(sum % 10,"sum % 10");
+//console.log(parseInt(string[targetIndex], 10) === sum % 10,"parseInt(string[targetIndex], 10) === sum % 10");
 return parseInt(string[targetIndex], 10) === sum % 10;
 
 });
