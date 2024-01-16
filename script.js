7aaee90679378d2430c57e5afe199592659f3892
r3conx <michutmpl@gmail.com>
2024-01-16 19:34:39 +0000

    16 sty 2024, 20:34

--- script.js
+++ script.js
@@ -144,6 +144,9 @@ let sum = 0;
 for (let k = sumStartIndex; k < string.length; k++) { console.log(k);
 if (k !== targetIndex) {
 sum += parseInt(string[k], 10);
+console.log(sum,"sum");
+console.log(string[k],"string[k]");
+console.log(parseInt(string[k], 10),"parseInt(string[k], 10)");
 }
 }
 return parseInt(string[targetIndex], 10) === sum % 10;
