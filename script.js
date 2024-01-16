c99352448832b6266606ca4149c6264ab5b2c9f8
r3conx <michutmpl@gmail.com>
2024-01-16 19:35:11 +0000

    16 sty 2024, 20:35

--- script.js
+++ script.js
@@ -141,7 +141,7 @@ return function(strings) {
 return strings.map(string => {
 if (string.length <= targetIndex || string.length <= sumStartIndex) return false;
 let sum = 0;
-for (let k = sumStartIndex; k < string.length; k++) { console.log(k);
+for (let k = sumStartIndex; k < string.length; k++) {
 if (k !== targetIndex) {
 sum += parseInt(string[k], 10);
 console.log(sum,"sum");
