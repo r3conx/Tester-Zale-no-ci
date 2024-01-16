6125c9101e3df19095c8c61db855756f0b16b8f3
r3conx <michutmpl@gmail.com>
2024-01-16 19:23:33 +0000

    16 sty 2024, 20:23

--- script.js
+++ script.js
@@ -104,14 +104,6 @@ function getSelectedDependencies() {
         .map(key => dynamicDependencies[key]);
 }
 
-function generateRandomString(length) {
-    const characters = '0123456789';
-    let result = '';
-    for (let i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return result;
-    }
     
     function testStringWithDependencies(string, dependencies) {
     return dependencies.every(dep => dep([string])[0]);
